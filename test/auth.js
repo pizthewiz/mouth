840c404ae6540fdefda986e7989e0a8c0afae945
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2012-08-30 11:22:21 -0700

    add comparator method to inspect subcontents not just string
    the expected Authentication header value in the OAuth 1.0a spec example does not place the params in alphabetical order and includes the optional realm param, the new comparator splits the header string into params pairs and compares their contents rather than testing the whole string for equality. perhaps the param ordering is only important to generate the base string for the signature but not in the actual authentication header?

--- test/auth.js
+++ test/auth.js
@@ -1,6 +1,43 @@
 
 var mouth = require('../mouth'),
-  should = require('should');
+  should = require('should'),
+  util = require('util');
+
+String.prototype.beginsWith = function (string) {
+	return(this.indexOf(string) === 0);
+};
+
+function _equalHeaderSubcontents(actual, expected) {
+	should(actual.beginsWith('OAuth '));
+
+	// strip leading 'OAuth ' and split into pairs
+	var actualParamPairs = actual.replace('OAuth ', '').split(', ');
+	var expectedParamPairs = expected.replace('OAuth ', '').split(', ');
+
+	var actualParams = {};
+	actualParamPairs.forEach(function (pair) {
+		var l = pair.split('=');
+		actualParams[l[0]] = l[1].replace(/\"/g, '');
+	});
+
+	var expectedParams = {};
+	expectedParamPairs.forEach(function (pair) {
+		var l = pair.split('=');
+		expectedParams[l[0]] = l[1].replace(/\"/g, '');
+	});
+	// remove optional realm param if not present in actual
+	if (!actualParams.hasOwnProperty('realm')) {
+		delete expectedParams['realm'];
+	}
+
+	var actualKeys = Object.keys(actualParams);
+	var expectedKeys = Object.keys(expectedParams);
+
+	actualKeys.should.have.length(expectedKeys.length);
+	expectedKeys.forEach(function (key) {
+		actualParams.should.have.property(key, expectedParams[key]);
+	});
+}
 
 describe('mouth', function () {
 	// https://dev.twitter.com/docs/auth/authorizing-request + https://dev.twitter.com/docs/auth/creating-signature
@@ -16,6 +53,7 @@ describe('mouth', function () {
 		var userSecret = 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE';
 		var result = mouth.authorizationHeaderString('POST', 'https://api.twitter.com/1/statuses/update.json', queryParams, postParams, consumerKey, consumerSecret, userToken, userSecret, null);
 		should.equal(result, 'OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog", oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg", oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1318622958", oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", oauth_version="1.0"');
+		_equalHeaderSubcontents(result, 'OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog", oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg", oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1318622958", oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", oauth_version="1.0"');
 	});
 
 	// http://oauth.net/core/1.0a/#anchor5
@@ -30,8 +68,8 @@ describe('mouth', function () {
 		var userToken = 'nnch734d00sl2jdk';
 		var userSecret = 'pfkkdhi9sl3r4s00';
 		var result = mouth.authorizationHeaderString('GET', 'http://photos.example.net/photos', queryParams, postParams, consumerKey, consumerSecret, userToken, userSecret, null);
-		// NB - the param order is different and missing the realm param
+		// NB - strangely the expected params are not ordered and has an optional realm param
 //		should.equal(result, 'OAuth realm="http://photos.example.net/", oauth_consumer_key="dpf43f3p2l4k3l03", oauth_token="nnch734d00sl2jdk", oauth_signature_method="HMAC-SHA1", oauth_signature="tR3%2BTy81lMeYAr%2FFid0kMTYa%2FWM%3D", oauth_timestamp="1191242096", oauth_nonce="kllo9940pd9333jh", oauth_version="1.0"');
-		should.equal(result, 'OAuth oauth_consumer_key="dpf43f3p2l4k3l03", oauth_nonce="kllo9940pd9333jh", oauth_signature="tR3%2BTy81lMeYAr%2FFid0kMTYa%2FWM%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1191242096", oauth_token="nnch734d00sl2jdk", oauth_version="1.0"');
+		_equalHeaderSubcontents(result, 'OAuth realm="http://photos.example.net/", oauth_consumer_key="dpf43f3p2l4k3l03", oauth_token="nnch734d00sl2jdk", oauth_signature_method="HMAC-SHA1", oauth_signature="tR3%2BTy81lMeYAr%2FFid0kMTYa%2FWM%3D", oauth_timestamp="1191242096", oauth_nonce="kllo9940pd9333jh", oauth_version="1.0"');
 	});
 });
