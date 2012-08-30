3ff8fbfda20c4d60d2db4429c978c9cc0e57b34e
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2012-08-29 22:30:27 -0700

    clear existing tests and add a twitter status update test

--- test/auth.js
+++ test/auth.js
@@ -1,181 +1,20 @@
 
 var mouth = require('../mouth'),
-  config = require('./config'),
-  should = require('should'),
-  querystring = require('querystring'),
-  util = require('util');
-
-function _verify(accessToken, accessTokenSecret, done) {
-  mouth.shit('GET', 'https://api.twitter.com/account/verify_credentials.json', {}, {}, null, null, config.twitter.consumerKey, config.twitter.consumerSecret, accessToken, accessTokenSecret, null, null, function (err, data, res) {
-    should.not.exist(err);
-    res.should.have.status(200);
-    res.should.be.json;
-//    data = JSON.parse(data);
-    done();
-  });
-}
-function _delete(tweetID, accessToken, accessTokenSecret, done) {
-    var queryParams = {};
-    var postParams = {};
-    var url = 'http://api.twitter.com/1/statuses/destroy/' + tweetID + '.json';
-    mouth.shit('POST', url, queryParams, postParams, null, null, config.twitter.consumerKey, config.twitter.consumerSecret, accessToken, accessTokenSecret, null, null, function (err, data, res) {
-      should.not.exist(err);
-      res.should.have.status(200);
-      res.should.be.json;
-      done();
-    });
-}
-function _updateAndDelete(accessToken, accessTokenSecret, done) {
-  var queryParams = {};
-  var postParams = {
-    'status': '{MOUTH TEST SUITE SLUG}'
-  };
-  mouth.shit('POST', 'http://api.twitter.com/1/statuses/update.json', queryParams, postParams, null, null, config.twitter.consumerKey, config.twitter.consumerSecret, accessToken, accessTokenSecret, null, null, function (err, data, res) {
-    should.not.exist(err);
-    res.should.have.status(200);
-    res.should.be.json;
-    data = JSON.parse(data);
-    should.exist(data.id_str);
-    var tweetID = data.id_str;
-
-    _delete(tweetID, accessToken, accessTokenSecret, done);
-  });
-}
+  should = require('should');
 
 describe('mouth', function () {
-  describe('term.ie', function () {
-    var credentials = {
-      accessToken: null,
-      accessTokenSecret: null
-    };
+	// https://dev.twitter.com/docs/auth/authorizing-request + https://dev.twitter.com/docs/auth/creating-signature
+	it('should construct the expected Authorization header for a Twitter status update', function () {
+		_getNonce= function(size) { return 'kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg'; };
+		_getTimestamp= function() { return '1318622958'; };
 
-    it('should GET a request token', function (done) {
-      mouth.shit('GET', 'http://term.ie/oauth/example/request_token.php', {}, {}, null, null, 'key', 'secret', null, null, null, null, function (err, data, res) {
-        should.not.exist(err);
-        res.should.have.status(200);
-
-        should.exist(data);
-        var parsedQuery = querystring.parse(data);
-        should.exist(parsedQuery.oauth_token);
-        should.exist(parsedQuery.oauth_token_secret);
-
-        credentials.accessToken = parsedQuery.oauth_token;
-        credentials.accessTokenSecret = parsedQuery.oauth_token_secret;
-
-        done();
-      });
-    });
-
-    it('should GET an access token', function (done) {
-      mouth.shit('GET', 'http://term.ie/oauth/example/access_token.php', {}, {}, null, null, 'key', 'secret', credentials.accessToken, credentials.accessTokenSecret, null, null, function (err, data, res) {
-        should.not.exist(err);
-        res.should.have.status(200);
-
-        should.exist(data);
-        var parsedQuery = querystring.parse(data);
-        should.exist(parsedQuery.oauth_token);
-        should.exist(parsedQuery.oauth_token_secret);
-
-        credentials.accessToken = parsedQuery.oauth_token;
-        credentials.accessTokenSecret = parsedQuery.oauth_token_secret;
-
-        done();
-      });
-    });
-
-    it('should make authenticated call', function (done) {
-      var queryParams = {
-        foo: 'bar',
-        dog: 'yes'
-      };
-      mouth.shit('GET', 'http://term.ie/oauth/example/echo_api.php', queryParams, {}, null, null, 'key', 'secret', credentials.accessToken, credentials.accessTokenSecret, null, null, function (err, data, res) {
-        should.not.exist(err);
-        res.should.have.status(200);
-
-        should.exist(data);
-        var parsedQuery = querystring.parse(data);
-        should.exist(parsedQuery.foo);
-        should.equal(parsedQuery.foo, queryParams.foo);
-        should.exist(parsedQuery.dog);
-        should.equal(parsedQuery.dog, queryParams.dog);
-
-        done();
-      });
-    });
-
-  });
-
-  describe('Twitter', function () {
-    var credentials = {
-      accessToken: null,
-      accessTokenSecret: null
-    };
-
-    // config
-    it('should have a valid config', function () {
-      should.exist(config.twitter.consumerKey);
-      should.exist(config.twitter.consumerSecret);
-      should.exist(config.twitter.slave.accessToken);
-      should.exist(config.twitter.slave.accessTokenSecret);
-      should.exist(config.twitter.indenturedServant.username);
-      should.exist(config.twitter.indenturedServant.password);
-    });
-
-    // slave
-    it('should verify slave credentials', function (done) {
-      _verify(config.twitter.slave.accessToken, config.twitter.slave.accessTokenSecret, done);
-    });
-
-    it('should return OK from help/test', function (done) {
-      var queryParams = {};
-      var postParams = {};
-      mouth.shit('GET', 'http://api.twitter.com/1/help/test.json', queryParams, postParams, null, null, config.twitter.consumerKey, config.twitter.consumerSecret, config.twitter.slave.accessToken, config.twitter.slave.accessTokenSecret, null, null, function (err, data, res) {
-        should.not.exist(err);
-        res.should.have.status(200);
-        res.should.be.json;
-        var data = JSON.parse(data);
-        should.equal(data, 'ok');
-
-        done();        
-      });
-    });
-
-    it('should update and delete status with slave credentials', function (done) {
-      _updateAndDelete(config.twitter.slave.accessToken, config.twitter.slave.accessTokenSecret, done);
-    });
-
-    // TODO - update with media and delete
-
-    // xAuth
-    it('should create credentials through xAuth and validate', function (done) {
-      var queryParams = {};
-      var postParams = {
-        'x_auth_username': config.twitter.indenturedServant.username,
-        'x_auth_password': config.twitter.indenturedServant.password,
-        'x_auth_mode': 'client_auth'
-      };
-      mouth.shit('POST', 'https://api.twitter.com/oauth/access_token', queryParams, postParams, null, null, config.twitter.consumerKey, config.twitter.consumerSecret, null, null, null, null, function (err, data, res) {
-        should.not.exist(err);
-        res.should.have.status(200);
-
-        should.exist(data);
-        var parsedQuery = querystring.parse(data);
-        should.exist(parsedQuery.oauth_token);
-        should.exist(parsedQuery.oauth_token_secret);
-
-        credentials.accessToken = parsedQuery.oauth_token;
-        credentials.accessTokenSecret = parsedQuery.oauth_token_secret;
-
-        done();
-      });
-    });
-
-    it('should verify xAuth credentials', function (done) {
-      _verify(credentials.accessToken, credentials.accessTokenSecret, done);
-    });
-
-//    it('should update and delete status with xAuth credentials', function (done) {
-//      _updateAndDelete(credentials.accessToken, credentials.accessTokenSecret, done);
-//    });
-  });
+		var queryParams = {'include_entities': true};
+		var postParams = {'status': 'Hello Ladies + Gentlemen, a signed OAuth request!'};
+		var consumerKey = 'xvz1evFS4wEEPTGEFPHBog';
+		var consumerSecret = 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw';
+		var userToken = '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb';
+		var userSecret = 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE';
+		var result = mouth.authorizationHeaderString('POST', 'https://api.twitter.com/1/statuses/update.json', queryParams, postParams, consumerKey, consumerSecret, userToken, userSecret);
+		should.equal(result, 'OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog", oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg", oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1318622958", oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", oauth_version="1.0"');
+	});
 });
